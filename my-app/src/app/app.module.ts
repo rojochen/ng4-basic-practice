2d40d4bdf02134f193b80d7b1ff77008a9136a0f
RojoChen <rojo@RojoChende-MacBook-Pro.local>
2017-11-20 22:52:40 +0800

    added component-demo-1

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ComponentDemo1Component } from './component-demo-1/component-demo-1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ComponentDemo1Component,
   ],
   imports: [
     BrowserModule
