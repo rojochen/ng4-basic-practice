a9ca51bb83a12cc60d072aacddead32c5393c2e7
RojoChen <rojo@RojoChende-MacBook-Pro.local>
2017-11-22 08:24:06 +0800

    fixed model demo

--- my-app/src/app/component-demo-3/component-demo-3.component.ts
+++ my-app/src/app/component-demo-3/component-demo-3.component.ts
@@ -5,10 +5,27 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './component-demo-3.component.html',
   styleUrls: ['./component-demo-3.component.css']
 })
+
 export class ComponentDemo3Component implements OnInit {
-
-  constructor() { }
-
+  oneWayValue: string ;
+  selectValue: string ;
+  twoWayValue: string ;
+  boxList: Array<{ selected: boolean; value: string }>;
+  constructor() {
+    this.oneWayValue = '';
+    this.selectValue = 'angular';
+    this.twoWayValue = '';
+    this.boxList = [];
+    this.boxList.push({selected: false, value: 'angular'});
+    this.boxList.push({selected: false, value: 'react'});
+    this.boxList.push({selected: false, value: 'vue'});
+  }
+  input(event) {
+    this.oneWayValue = event.target.value;
+  }
+  log() {
+    console.log(this.twoWayValue );
+  }
   ngOnInit() {
   }
 
