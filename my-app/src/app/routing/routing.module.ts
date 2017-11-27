a949e372f0373c7a264f76e8143bb0acd8db33f2
louisxie <louisxie@truetel.com>
2017-11-27 15:49:30 +0800

    👷

--- my-app/src/app/routing/routing.module.ts
+++ my-app/src/app/routing/routing.module.ts
@@ -11,7 +11,7 @@ const routes: Routes = [
   { path: 'login', component: RouteDemo1Component },
   { path: 'index', component: RouteDemo2Component },
   { path: '**', component: RouteDemo3Component }
-];
+]; 
 
 @NgModule({
   imports: [
