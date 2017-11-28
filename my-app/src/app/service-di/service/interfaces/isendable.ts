5508a7b7d49683f0a367b5b60e0b16565f7f1919
RojoChen <rojo@RojoChende-MacBook-Pro.local>
2017-11-29 00:29:31 +0800

    added abstract

--- my-app/src/app/service-di/service/interfaces/isendable.ts
+++ my-app/src/app/service-di/service/interfaces/isendable.ts
@@ -1,3 +1,10 @@
 export interface  ISendable {
      sendMessage(): string;
 }
+abstract class MySender implements ISendable {
+    abstract sendMessage(): string;
+    send(): string {
+        console.log('send message');
+        return this.sendMessage();
+    }
+}
