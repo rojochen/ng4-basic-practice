a3932d1acceabe2cae15c8c57722c754bacd991c
RojoChen <rojo@RojoChende-MacBook-Pro.local>
2017-11-29 00:32:17 +0800

    added Mysender

--- my-app/src/app/service-di/service/interfaces/isendable.ts
+++ my-app/src/app/service-di/service/interfaces/isendable.ts
@@ -1,10 +1,16 @@
 export interface  ISendable {
      sendMessage(): string;
 }
-abstract class MySender implements ISendable {
+abstract class AbstractSender implements ISendable {
     abstract sendMessage(): string;
     send(): string {
         console.log('send message');
         return this.sendMessage();
+    }
+}
+class MySender extends AbstractSender {
+    sendMessage(): string {
+        console.log('my message');
+        return 'my message';
     }
 }
