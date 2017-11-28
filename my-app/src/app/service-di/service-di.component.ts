44755d4850d620d4f8fd62784f41eb210acac775
Louis <neighborhood0830@gmail.com>
2017-11-28 23:52:53 +0800

    chnage service

--- my-app/src/app/service-di/service-di.component.ts
+++ my-app/src/app/service-di/service-di.component.ts
@@ -2,13 +2,14 @@ import { Component, OnInit } from '@angular/core';
 import { NotificationService } from './service/notification/notification.service';
 import { ISendable } from './service/interfaces/isendable';
 import { SmsServiceProviderService } from './service/sms/sms-service-provider.service';
+import { AwsSmsService } from './service/sms/aws-sms.service';
 @Component({
   selector: 'app-service-di',
   templateUrl: './service-di.component.html',
   styleUrls: ['./service-di.component.css'],
   providers: [
     NotificationService,
-    { provide: ISendable, useClass: SmsServiceProviderService }
+    { provide: ISendable, useClass: AwsSmsService }
   ]
 
 })
@@ -18,7 +19,6 @@ export class ServiceDiComponent implements OnInit {
 
   ngOnInit() {
     this.message = this.notificationService.showMessage();
-    console.log('this.title: ', this.title);
   }
 
 }
