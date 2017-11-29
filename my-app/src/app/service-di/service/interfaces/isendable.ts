0d2cc8f699b25f397917c5841aa1a0601d5ad20c
louisxie <louisxie@truetel.com>
2017-11-29 10:34:46 +0800

    reset code.

--- my-app/src/app/service-di/service/interfaces/isendable.ts
+++ my-app/src/app/service-di/service/interfaces/isendable.ts
@@ -1,16 +1,3 @@
 export interface  ISendable {
      sendMessage(): string;
 }
-abstract class AbstractSender implements ISendable {
-    abstract sendMessage(): string;
-    send(): string {
-        console.log('send message');
-        return this.sendMessage();
-    }
-}
-class MySender extends AbstractSender {
-    sendMessage(): string {
-        console.log('my message');
-        return 'my message';
-    }
-}
