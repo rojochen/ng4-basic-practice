e1df9fb75204a7445be7a6cc17757a8465bf0a95
louisxie <louisxie@truetel.com>
2017-11-30 11:05:28 +0800

    Upgrade HTTP sample code.

--- my-app/src/app/service/service-demo-2/service-demo-2.component.ts
+++ my-app/src/app/service/service-demo-2/service-demo-2.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { LoggerService } from '../provider/logger.service';
 import { ApiService } from '../provider/api.service';
 import { Observable } from 'rxjs/Observable';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 
 import 'rxjs/add/observable/forkJoin';
 interface ItemsResponse {
@@ -10,6 +10,23 @@ interface ItemsResponse {
   id: number;
   title: string;
   body: string;
+}
+class ItemResClass {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+class OtherPartsOfTheRequest {
+  constructor() {
+  }
+  setHeader(HTTP, reqBody, reqHeader) {
+    return HTTP.post('https://jsonplaceholder.typicode.com/posts', reqBody, reqHeader);
+  }
+  setParams(HTTP, reqBody, reqParams) {
+    return HTTP.post('https://jsonplaceholder.typicode.com/posts', reqBody, reqParams);
+  }
 }
 @Component({
   selector: 'app-service-demo-2',
@@ -24,67 +41,97 @@ export class ServiceDemo2Component implements OnInit {
   get: any[];
   getAsync: any;
   getPromise: any[];
+  request: any;
+  otherPartsOfTheRequest: any;
   constructor(
     // private Log: LoggerService //這邊無法使用LogService 因為沒有在這邊inject providers
     @Inject('API_URL') private apiDomain: string, // useValue
     private api: ApiService,
     private http: HttpClient
-  ) { }
+  ) {
+  }
 
   ngOnInit() {
     // this.Log.debug('Service demo 2'); 這邊無法使用LogService 因為沒有在這邊inject
 
+    this.request = {
+      method: 'POST',
+      body: JSON.stringify({
+        title: 'foo',
+        body: 'bar',
+        userId: 1
+      }),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8'
+      }
+    };
+    // this.http add interface
     this.http.get<ItemsResponse>(this.apiDomain + '/posts?userId=1').toPromise().then(data => {
       console.log(data.body);
     });
 
-    this.http.get<ItemsResponse>(this.apiDomain + '/posts?userId=1').toPromise().then(data => {
-      console.log(data.userId);
+    // this.http add class
+    this.http.get<ItemResClass[]>(this.apiDomain + '/posts?userId=1').toPromise().then(data => {
+      console.log('data: ', data);
     }, (err: HttpErrorResponse) => {
       console.log('err: ', err);
       console.log('Something went wrong!');
     });
-
 
     this.api.get(this.apiDomain + '/posts').subscribe(res => {
       console.log('res type json: ', typeof res);
       this.get = res;
     });
 
+    // pipe Async
     this.getAsync = this.api.get(this.apiDomain + '/posts');
 
     // es6 標準promise的寫法
     this.api.getTypeText(this.apiDomain + '/posts').subscribe(res => {
       console.log('res type text: ', typeof res);
     });
-
+    // Format Promise
     this.api.getPromise(this.apiDomain + '/posts').then(res => {
       this.getPromise = res;
     });
 
-    setTimeout(() => {
-      this.api.post('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-          title: 'foo',
-          body: 'bar',
-          userId: 1
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8'
-        }
-      }).subscribe(res => {
-        console.log('res', res);
-      });
-    }, 1000);
+
+    this.api.post('https://jsonplaceholder.typicode.com/posts', this.request).subscribe(res => {
+      console.log('res', res);
+    });
+
+    // Rxjs promise.all
     this.forkJoin();
+
+    // many Requests
+    this.manyRequests();
+
+    // otherPartsOfTheRequest
+    this.otherPartsOfTheRequest = new OtherPartsOfTheRequest();
+    this.otherPartsOfTheRequest.setHeader(this.http, this.request, {
+      headers: new HttpHeaders().set('Authorization', 'my-auth-token'),
+    }).subscribe(res => {
+    });
+
+    this.otherPartsOfTheRequest.setParams(this.http, this.request, {
+      params: new HttpParams().set('id', '3'),
+    }).subscribe(res => {
+    });
   }
-  // forkJoin = promise.all([])
+  // Rxjs forkJoin = promise.all([])
   forkJoin() {
     Observable.forkJoin([this.api.get(this.apiDomain + '/posts'), this.api.get(this.apiDomain + '/posts')])
       .subscribe(results => {
         console.error('results: ', results);
       });
+  }
+  manyRequests() {
+    const req = this.api.post('https://jsonplaceholder.typicode.com/posts', this.request);
+    // 0 requests made - .subscribe() not called.
+    req.subscribe();
+    // 1 request made.
+    req.subscribe();
+    // 2 requests made.
   }
 
 }
