46e886706e0fd21cb58e40181e04fbfeca7e8ca2
louisxie <louisxie@truetel.com>
2017-12-12 15:15:59 +0800

    queryParams

--- my-app/src/app/route/dynamic3/dynamic3-parent/dynamic3-parent.component.ts
+++ my-app/src/app/route/dynamic3/dynamic3-parent/dynamic3-parent.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-dynamic3-parent',
   templateUrl: './dynamic3-parent.component.html',
   styleUrls: ['./dynamic3-parent.component.css']
 })
 export class Dynamic3ParentComponent implements OnInit {
-
-  constructor() { }
+  pageId: string;
+  sub:any;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.sub = this.activatedRoute.queryParams.subscribe(params => {
+      console.log('params: ', params);
+      this.pageId = params.page || 0;
+    });
+  }
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+
   }
 
 }
--- my-app/src/app/route/dynamic3/dynamic3.module.ts
+++ my-app/src/app/route/dynamic3/dynamic3.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { Dynamic3ParentComponent } from '../dynamic3/dynamic3-parent/dynamic3-parent.component';
-import { Dynamic3ChildComponent } from '../dynamic3/dynamic3-child/dynamic3-child.component';
 const routes: Routes = [
-  { path: 'dynamic3/parent', component: Dynamic3ParentComponent },
-  { path: 'dynamic3/child/:id', component: Dynamic3ChildComponent }
+  { path: 'dynamic3/parent', component: Dynamic3ParentComponent }
 ];
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-  declarations: [Dynamic3ParentComponent, Dynamic3ChildComponent],
+  declarations: [Dynamic3ParentComponent],
   exports: [RouterModule]
 })
 export class Dynamic3Module { }
