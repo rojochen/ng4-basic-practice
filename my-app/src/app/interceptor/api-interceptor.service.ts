4c75a628b0973f3857450ef51e9bc486c1376562
RogerChen <jjc.roger@gmail.com>
2017-12-18 20:29:09 +0800

    for testing

--- my-app/src/app/interceptor/api-interceptor.service.ts
+++ my-app/src/app/interceptor/api-interceptor.service.ts
@@ -7,7 +7,7 @@ export class ApiInterceptorService implements HttpInterceptor {
 
   constructor() { }
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    //表頭塞token
+    //表頭塞token  HHH
     const authReq = req.clone({headers: req.headers.set('Authorization', 'my-auth-token')});
     console.log('authReq: ', authReq);
     return next.handle(authReq);
