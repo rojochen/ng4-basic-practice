f6bd4b52f5791d3676a38f726c8a00893c10b170
Louis <neighborhood0830@gmail.com>
2017-12-23 13:57:06 +0800

    create collar directive.

---
+++ my-app/src/app/directive/collar.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CollarDirective } from './collar.directive';
+
+describe('CollarDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CollarDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ my-app/src/app/directive/collar.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appCollar]'
+})
+export class CollarDirective {
+
+  constructor() { }
+
+}
