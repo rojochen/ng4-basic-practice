8e2ff15c6a5ce32c9bd76ed32407079b71296fc6
louisxie <louisxie@truetel.com>
2018-01-15 15:57:30 +0800

    fix Header and Params

--- my-app/src/app/ajax/ajax-demo-2/ajax-demo-2.component.ts
+++ my-app/src/app/ajax/ajax-demo-2/ajax-demo-2.component.ts
@@ -3,6 +3,14 @@ import { Observable } from 'rxjs/Observable';
 import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Subscription } from 'rxjs/Subscription';
 
+interface Store {
+  "id": string
+  "petId": string;
+  "quantity": number;
+  "shipDate": Date;
+  "status": string;
+  "complete": boolean;
+}
 @Component({
   selector: 'app-ajax-demo-2',
   templateUrl: './ajax-demo-2.component.html',
@@ -12,6 +20,7 @@ export class AjaxDemo2Component implements OnInit {
   addStoreId: string;
   searchStoreId: string;
   deleteStoreId: string;
+  storeBody: Store;
   constructor(
     private Http: HttpClient
   ) { }
@@ -19,24 +28,40 @@ export class AjaxDemo2Component implements OnInit {
   ngOnInit() {
   }
   addData(): void {
-    this.Http.post('http://petstore.swagger.io/v2/store/order', {
+    this.storeBody = {
       "id": this.addStoreId,
       "petId": this.addStoreId,
       "quantity": 0,
       "shipDate": new Date(),
       "status": "placed",
       "complete": false
-    }).subscribe(res => {
+    };
+    this.Http.post<Store>('http://petstore.swagger.io/v2/store/order', this.storeBody).subscribe(res => {
       console.log('res: ', res);
-    })
+    });
   }
   searchData(): void {
     this.Http.get(`http://petstore.swagger.io/v2/store/order/${this.searchStoreId}`).subscribe(res => {
       console.log('res: ', res);
-    })
+    });
   }
   deleteData(): void {
     this.Http.delete(`http://petstore.swagger.io/v2/store/order/${this.deleteStoreId}`).subscribe(res => {
+      console.log('res: ', res);
+    });
+  }
+
+  callApiWhenSendParams(): void {
+    this.Http.get(`http://petstore.swagger.io/v2/store/order/2`, {
+      params: new HttpParams().set('id', '3'),
+    }).subscribe(res => {
+      console.log('res: ', res);
+    });
+  }
+  callApiWhenSendHeader(): void {
+    this.Http.get(`http://petstore.swagger.io/v2/store/order/2`, {
+      headers: new HttpHeaders().set('Authorization', 'my-auth-token'),
+    }).subscribe(res => {
       console.log('res: ', res);
     });
   }
